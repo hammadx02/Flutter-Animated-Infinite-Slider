62f71f5ecd8820fea79dacb596f6e8652d8f65d5
hammadx02 <hammadaali02@gmail.com>
2024-07-30 22:47:17 +0500

    adding drag

---
+++ lib/features/home/presentation/widgets/dragable_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DragableWidget extends StatefulWidget {
+  const DragableWidget({super.key});
+
+  @override
+  State<DragableWidget> createState() => _DragableWidgetState();
+}
+
+class _DragableWidgetState extends State<DragableWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
